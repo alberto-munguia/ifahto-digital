84f8175e166eff5c3209385db9465cef1e2f50b4
Alberto Munguia <munguiaolmos.alberto@gmail.com>
2020-03-23 18:14:07 -0600

    se retiran plugins

--- webroot/webpack/src/plugins/vue-bootstrap-table.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VueBootstrap4Table from 'vue-bootstrap4-table'
-
-Vue.component('VueBootstrap4Table', VueBootstrap4Table)
--- webroot/webpack/src/plugins/vue-moment.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-const Vuemoment = require('vue-moment')
-
-Vue.use(Vuemoment)
